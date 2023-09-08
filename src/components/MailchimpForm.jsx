44af4a9d1c8d426ac0d8b67577a338fdda41ac81
Vaibhav Singh <144379437+owasptcet@users.noreply.github.com>
2023-09-08 23:02:46 +0530

    working code

--- src/components/MailchimpForm.jsx
+++ src/components/MailchimpForm.jsx
@@ -1,22 +1,22 @@
-// import MailchimpSubscribe from "react-mailchimp-subscribe";
-// import Newsletter from "./Newsletter";
+import MailchimpSubscribe from "react-mailchimp-subscribe";
+import Newsletter from "./Newsletter";
 
-// function MailchimpForm() {
-//   const postUrl = `${process.env.REACT_APP_MAILCHIMP_URL}?u=${process.env.REACT_APP_MAILCHIMP_U}&id=${process.env.REACT_APP_MAILCHIMP_ID}`;
+function MailchimpForm() {
+  const postUrl = `${process.env.REACT_APP_MAILCHIMP_URL}?u=${process.env.REACT_APP_MAILCHIMP_U}&id=${process.env.REACT_APP_MAILCHIMP_ID}`;
 
-//   return (
-//     <>
-//       <MailchimpSubscribe
-//         url={postUrl}
-//         render={({ subscribe, status, message }) => (
-//           <Newsletter
-//             status={status}
-//             message={message}
-//             onValidated={(formData) => subscribe(formData)}
-//           />
-//         )}
-//       />
-//     </>
-//   );
-// }
-// export default MailchimpForm;
+  return (
+    <>
+      <MailchimpSubscribe
+        url={postUrl}
+        render={({ subscribe, status, message }) => (
+          <Newsletter
+            status={status}
+            message={message}
+            onValidated={(formData) => subscribe(formData)}
+          />
+        )}
+      />
+    </>
+  );
+}
+export default MailchimpForm;
