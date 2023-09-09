1e073796c9e5b7d7b0b22f80879a915eb953e3dd
Amxn-2 <amansharmaa1205@gmail.com>
2023-09-09 13:17:57 +0530

    Configure React app for deployment to GitHub Pages

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import { Container, Row, Col } from "react-bootstrap";
 import MailchimpForm from "./MailchimpForm";
-import logo from "../assets/img/logo.svg";
+import logo from "../assets/img/logo.png";
 import navIcon1 from "../assets/img/nav-icon1.svg";
 import navIcon2 from "../assets/img/nav-icon2.svg";
 import navIcon3 from "../assets/img/nav-icon3.svg";
@@ -16,17 +16,14 @@ function Footer() {
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="#">
-                <img src={navIcon1} alt="Icon" />
+              <a href="https://www.linkedin.com/company/owasp-tcet/">
+                <img src={navIcon1} alt="LinkedIn" />
               </a>
-              <a href="#">
-                <img src={navIcon2} alt="Icon" />
-              </a>
-              <a href="#">
-                <img src={navIcon3} alt="Icon" />
+              <a href="https://www.instagram.com/owasptcet/">
+                <img src={navIcon3} alt="Instagram" />
               </a>
             </div>
-            <p>Copyright 2022. All Rights Reserved</p>
+            <p>Copyright 2023. All Rights Reserved</p>
           </Col>
         </Row>
       </Container>
